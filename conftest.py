6d6b85ea7a2522935ea535e2eeb66582c22f744c
timliakh <barkaloft@gmail.com>
2026-06-03 21:17:39 +0300

    Added need_review marker and updated conftest.py

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 from selenium import webdriver
 
 
--- test_product_page.py
+++ test_product_page.py
@@ -41,7 +41,8 @@ class TestUserAddToBasketFromProductPage():
         page = ProductPage(browser, link)
         page.open()
         page.should_not_be_success_message()
-
+    
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear2019"
         page = ProductPage(browser, link)
@@ -73,6 +74,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -82,6 +84,15 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page.should_be_login_page()
 
 
+@pytest.mark.need_review
+@pytest.mark.parametrize("link", links)
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+
+
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
